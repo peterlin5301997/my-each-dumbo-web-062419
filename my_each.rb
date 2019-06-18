1fc0314fae5148f1c09fcd9b2b4a78826625bdca
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 18:35:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,10 @@
 def my_each(array) # put argument(s) here
   # code here
   count = 0
+  newArray = []
   while count < array.length do
-    return array[count]
+    newArray += array[count]
     count += 1
   end
+  return newArray
 end
